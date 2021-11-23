e4f5cd370f1b20faccfa8a3beeedc1289c4822a1
Guilherme Carvalho <kenny-300@outlook.com>
2021-11-23 13:48:22 -0300

    Change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('change message'));
+app.get('/', (req, res) => res.send('Joao é o meu salvador'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
